9ca409989b42dd5cf8a8d3791ffd52659e0c2c03
Alongkots <alongkots@Alongkotss-MacBook-Pro.local>
2019-12-15 01:10:05 +0700

    change app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React V4 on feature branch 
+          Learn React V4 on feature branch======
         </a>
       </header>
     </div>
